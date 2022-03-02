92c3af1eb9567b80310d786936e99685b309421e
Ivan Stefanovski <istefanovski46@gmail.com>
2022-03-02 18:29:18 +0100

    Change

--- src/com/company/First.java
+++ src/com/company/First.java
@@ -3,5 +3,6 @@ package com.company;
 public class First {
     public static void main(String[] args) {
         System.out.println("Master branch");
+        System.out.println("Promena");
     }
 }
